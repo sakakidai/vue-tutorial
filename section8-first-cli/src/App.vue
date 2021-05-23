c59404f4597981f67592750c45260d5c27646d57
sakakidai <sd.lybd1016@gmail.com>
2021-05-23 14:24:23 +0900

    update section8

--- section8-first-cli/src/App.vue
+++ section8-first-cli/src/App.vue
@@ -3,9 +3,19 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        v-for="friend in friends"
+        :id="friend.id"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      ></friend-contact>
     </ul>
   </section>
 </template>
@@ -20,16 +30,38 @@ export default {
           name: 'Manuel Lorenz',
           phone: '0123 45678 90',
           email: 'manuel@localhost.com',
+          isFavorite: true,
         },
         {
           id: 'hulie',
           name: 'Julie Jones',
           phone: '0987 654421 21',
           email: 'julie@localhost.com',
+          isFavorite: false,
         }
       ]
     }
   },
+
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(friend => friend.id === friendId);
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      }
+      this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId) {
+      this.friends = this.friends.filter(friend => friend.id !== friendId);
+    }
+  }
 }
 </script>
 
@@ -66,7 +98,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -100,4 +133,19 @@ header {
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
 
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
+}
 </style>
