b9c461870edf0023c94757a129990f97e94e42c3
sakakidai <sd.lybd1016@gmail.com>
2021-06-05 15:10:09 +0900

    update section19

--- section19-auth/src/pages/auth/UserAuth.vue
+++ section19-auth/src/pages/auth/UserAuth.vue
@@ -1,21 +1,29 @@
 <template>
-  <form @submit.prevent="submitForm">
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
     <base-card>
-      <div class="form-control">
-        <label for="emial">E-Mail</label>
-        <input type="email" id="email" v-model="email">
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="password">
-      </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password (must be at least 6 characters long).
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p
+          v-if="!formIsValid"
+        >Please enter a valid email and password (must be at least 6 characters long).</p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
+      </form>
     </base-card>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -26,9 +34,10 @@ export default {
       password: '',
       formIsValid: true,
       mode: 'login',
-    }
+      isLoading: false,
+      error: null,
+    };
   },
-
   computed: {
     submitButtonCaption() {
       if (this.mode === 'login') {
@@ -45,9 +54,8 @@ export default {
       }
     },
   },
-
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (
         this.email === '' ||
@@ -58,14 +66,26 @@ export default {
         return;
       }
 
-      if (this.mode === 'login') {
-        // ...
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password,
-        });
+      this.isLoading = true;
+
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
+
+      try {
+        if (this.mode === 'login') {
+          await this.$store.dispatch('login', actionPayload);
+        } else {
+          await this.$store.dispatch('signup', actionPayload);
+        }
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        this.$router.replace(redirectUrl);
+      } catch (err) {
+        this.error = err.message || 'Failed to authenticate, try later.';
       }
+
+      this.isLoading = false;
     },
     switchAuthMode() {
       if (this.mode === 'login') {
@@ -74,8 +94,11 @@ export default {
         this.mode = 'login';
       }
     },
-  }
-}
+    handleError() {
+      this.error = null;
+    },
+  },
+};
 </script>
 
 <style scoped>
--- section19-auth/src/store/index.js
+++ section19-auth/src/store/index.js
@@ -2,14 +2,14 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
-import authModule from './modules/auth/index';
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
     requests: requestsModule,
-    auth: authModule,
-  },
+    auth: authModule
+  }
 });
 
 export default store;
